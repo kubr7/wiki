7e4d41b5e80effe621c51a88ed6b3966ab0ac3dc
kubr7 <kubrtech@gmail.com>
2024-11-14 03:14:25 +0530

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if[[ $CREATE_SUPERUSER ]];
-then
+
+
+if [[ $CREATE_SUPERUSER ]]; then
      python manage.py createsuperuser --no-input --username kubr --email kubrtech@gmail.com
 fi
